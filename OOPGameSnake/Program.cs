5f548e322bf0a163323e6b60c2264bd7e27c1401
Vladislav Kuznetsov <vlad.kuznetsov.97@gmail.com>
2019-09-06 12:22:56 +0300

    Repository initialization

---
+++ OOPGameSnake/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DataStruct;
+using NConsoleGraphics;
+
+namespace OOPGameSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+            a.Add(1);
+            a.Add(2);
+            a.Add(3);
+
+            var b = a.ToArray();
+
+            foreach (var c in b )
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
